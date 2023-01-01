13a4e01edda767ede8fb50e70a19a564600564a3
cecil <cecilghimire@gmail.com>
2023-01-01 16:53:26 +0545

    added open and close button

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -1,6 +1,12 @@
 import { motion } from "framer-motion";
-import React from "react";
-import { AiFillAlert, AiFillSignal } from "react-icons/ai";
+import React, { useState } from "react";
+import {
+  AiFillAlert,
+  AiFillSignal,
+  AiFillEnvironment,
+  AiOutlineDoubleLeft,
+  AiOutlineDoubleRight,
+} from "react-icons/ai";
 import "../App.css";
 
 const routes = [
@@ -9,22 +15,53 @@ const routes = [
     icon: <AiFillAlert />,
   },
   {
+    name: "Events",
+    icon: <AiFillEnvironment />,
+  },
+  ,
+  {
     name: "Charts",
     icon: <AiFillSignal />,
   },
 ];
 
 export const Sidebar = ({ children }) => {
+  const [sideBarOpen, setsideBarOpen] = useState(
+    <AiOutlineDoubleLeft size={10} />
+  );
+  const handlerSideBarOpen = () => {
+    if (sideBarOpen === <AiOutlineDoubleLeft size={10} />) {
+      setsideBarOpen(<AiOutlineDoubleRight size={10} />);
+    } else if (sideBarOpen === <AiOutlineDoubleRight size={10} />);
+    {
+      setsideBarOpen(<AiOutlineDoubleLeft size={10} />);
+    }
+  };
+
   return (
     <div className="sidebar-container">
       <motion.div animate={{ width: "500px" }} className="side-bar">
-        <div className="flex p-2 border">
+        <div className="bg-indigo-900 text-sm text-white px-2 py-3">
+          <p className="pl-2">Lalitpur District Disaster Portal</p>
+        </div>
+        <div
+          className="text-white flex justify-end border-rounded cursor-pointer"
+          onClick={handlerSideBarOpen}
+        >
+          <div className="bg-indigo-900 m-1 rounded-full p-2">
+            {" "}
+            {sideBarOpen}
+          </div>
+        </div>
+        <div className="flex p-3 border">
           {routes.map((index) => {
             return (
               <>
-                <div className="p-2 mr-5 ">
-                  <span>{index.icon}</span>
-                  <p className="text-xs">{index.name}</p>
+                <div className="p-5 mr-2  border-t-2 border-indigo-600 hover:bg-gray-400 hover:text-white cursor-pointer">
+                  <center>
+                    <span>{index.icon}</span>
+                  </center>
+                  <p className="text-[10px] pt-1">{index.name}</p>
                 </div>
               </>
             );
