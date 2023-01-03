93a726d291cd7040a8350ea4be9c456a99bba2e3
cecil <cecilghimire@gmail.com>
2023-01-03 13:35:08 +0545

    added more option in dash board and handlefunctions

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -14,27 +14,54 @@ import { MainMap } from "../MapElements/MainMap";
 import L from "leaflet";
 import {
   AiFillAlert,
-  AiFillSignal,
+  AiFillSignal,AiOutlineLogin,
   AiFillEnvironment,
   AiOutlineDoubleLeft,
   AiOutlineDoubleRight,
 } from "react-icons/ai";
+import {MdSpaceDashboard,MdCellWifi} from 'react-icons/md'
+import {RiProfileLine} from 'react-icons/ri'
+import {GoReport} from 'react-icons/go'
 import { Legend } from "./Legend";
+
+const handleDashboard=()=>{}
+const handleLiveData=()=>{}
+const handleCharts=()=>{}
+const handleReport=()=>{}
+const handleLogin=()=>{}
+const handleProfile=()=>{}
+const handleWebServices=()=>{}
 
 const routes = [
   {
-    name: "Alert",
-    icon: <AiFillAlert size={24} />,
+    name: "Dashboard",
+    icon: <MdSpaceDashboard size={19} />,
   },
   {
-    name: "Events",
-    icon: <AiFillEnvironment size={24} />,
+    name: "Live Data",
+    icon: <MdCellWifi size={19} />,
   },
   ,
   {
     name: "Charts",
-    icon: <AiFillSignal size={24} />,
+    icon: <AiFillSignal size={19} />,
   },
+  {
+    name: "Report an event",
+    icon: <GoReport size={19} />,
+  },
+  {
+     name: "Login",
+     icon: <AiOutlineLogin size={19} />,
+  },
+  {
+    name: "Profile",
+    icon: <RiProfileLine size={19} />,
+ },
+ {
+  name: "Web Services",
+  icon: <RiProfileLine size={19} />,
+},
 ];
 
 export const Dashboard = () => {
@@ -53,14 +80,14 @@ export const Dashboard = () => {
           <Sidebar setresizer={setresizer} resizer={resizer} />
         </div>
 
-        <div className="flex flex-col justify-center text-white w-24 bg-indigo-900 ">
+        <div className="flex flex-col justify-center text-white  bg-indigo-900 ">
           {routes.map((index) => {
             return (
               <center>
                 <div
                   key={index.name}
-                  className="p-5 border-b-2 bg-indigo-900 border-white-600 hover:bg-indigo-800 hover:text-white cursor-pointer"
-                >
+                  className="p-2 border-b-2 bg-indigo-900 border-white-600 hover:bg-indigo-800 hover:text-white cursor-pointer"
+                onClick={routes.handleClick}>
                   <span>{index.icon}</span>
                   <p className="text-[10px] pt-1">{index.name}</p>
                 </div>
