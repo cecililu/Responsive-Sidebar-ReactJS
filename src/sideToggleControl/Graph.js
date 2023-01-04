9ac0e014a0f9d806459b05274b53e96157b04bcb
cecil <cecilghimire@gmail.com>
2023-01-04 21:06:22 +0545

    chart section  css fix

--- src/sideToggleControl/Graph.js
+++ src/sideToggleControl/Graph.js
@@ -75,10 +75,23 @@ export const Graph = () => {
   }, []);
 
   return (<div className='mt-10'>
-  
-  <div className="bg-gray-200 border-b py-3 flex justify-center">
-           <span> Data of the Occurance of event</span>
+     <div className=" flex flex-col p-2 bg-gray-100 mt-9 py-5" >
+            <div className="text-gray-700 text-[11px]">
+              Date: 2022/2/3- 2022/4/3
+            </div>
+            <div className="text-gray-700 text-[11px] ">
+              <span className='mr-3'>Source:</span>
+              
+                <p className=" inline px-2 pb-1 bg-red-700 text-white text-[11px] rounded-lg w-1/3">
+                  As of Today
+                </p>
+              
+            </div>
           </div>
-  <Chart ref={chartRef} type='line' data={chartData} />
-  </div>);
+  <div className="bg-indigo-900 text-sm text-white py-1 flex justify-center">
+            Data of the Occurance of event
+          </div>
+
+  <div className='p-7 bg-gray-100'><Chart ref={chartRef} type='line' data={chartData} />
+  </div></div>);
 }
