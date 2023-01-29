3f047f773ec50eb130e5c8c8f1fb8f1c5b076284
cecil <cecilghimire@gmail.com>
2023-01-29 19:26:46 +0545

    revert

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -1,54 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { setComponent } from './alsice';
+import React from 'react'
+import { Dashboard } from './Dashboard'
 
-
-export const Comp1 = () => {
-  return (
-    <div>Comp1 redenre</div>
-  )
-}
-
-export const Comp2 = () => {
-  return (
-    <div>Comp2 rendered</div>
-  )
-}
 
 export const MainPage = () => {
-  const component = useSelector(state => state.component);
-
-
-  const [Comp,setcomp]=useState('no')
-  const dispatch = useDispatch();
-  
-  console.log(component)
-  console.log(Comp)
-  useEffect(()=>{
-    console.log('useeffect')
-    if (Comp=='Comp1'){
-      setComponent(<Comp1/>)
-    }
-    if (Comp=='Comp2'){
-      setComponent(<Comp2/>)
-    }
-  },[])
-
-  const handleClick = (e) => {
-    const { target: { id } } = e;
-    dispatch(setComponent(id));
-  }
-
   return (
-    <>    <div className='flex'>
-      Main page----
-      <button id='Comp1' onClick={handleClick}>Render Comp1</button>
-      <button id='Comp2' onClick={handleClick}>Render Comp2</button>
-    
-    </div>
-    *{component}*
-    {Comp}
-    </>
-
+    <div className=''>
+        <Dashboard/>
+            
+   </div>
   )
 }
+
